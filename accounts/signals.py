088da08420f68584b5c496f34ffe40de0947769c
Ma-Singo <codesingo@gmail.com>
2025-12-27 01:47:13 +0100

    Adding a fail login signal

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,6 +1,13 @@
 from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.conf import settings
+from django.db.models.signals import post_save
+from django.contrib.auth.signals import user_login_failed
+from django.contrib.auth import get_user_model
+
+from notifications.tasks import send_welcome_email, send_notification_task
+from notifications.services.payload import NotificationPayload
+from notifications.services.notification_service import NotificationService
 
 from allauth.account.signals import (
     user_signed_up as allauth_user_signed_up,
@@ -11,6 +18,8 @@ from allauth.account.signals import (
     email_removed as allauth_email_removed,
     
 )
+
+User = get_user_model()
 
 from allauth.socialaccount.signals import (
     pre_social_login,
@@ -37,3 +46,21 @@ def user_signed_up_handler(request, user, *args, **kwargs):
         recipient_list=[user.email], 
         fail_silently=False
     )
+
+@receiver(post_save, sender=User)
+def user_created(sender, instance, created, **kwargs):
+    if created:
+        key = f"user-created:{instance.id}"
+        send_welcome_email(instance.id, key)
+
+@receiver(user_login_failed)
+def failed_login(sender, credentials, **kwargs):
+    payload = NotificationPayload(
+        user_id=None,
+        channel="email",
+        subject="Failed login attempt",
+        body="Someone tried to log into your account.",
+        metadata={"email": credentials.get("username")}
+    )
+
+    NotificationService.send(None, payload, "failed-login")
