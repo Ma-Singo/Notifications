ca58fd70dd585cadc18a4946822b346cb549dadb
Ma-Singo <codesingo@gmail.com>
2026-02-19 20:16:41 +0100

    feat(authentication): Added an authenticated route for user

--- auth/user_token.py
+++ auth/user_token.py
@@ -1,19 +1,23 @@
-from fastapi import Depends
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-
 from pwdlib import PasswordHash
 from datetime import datetime, timedelta, timezone
 from typing import Annotated
 import jwt
+from jwt.exceptions import InvalidTokenError, ExpiredSignatureError, DecodeError
 
 from core.config import get_settings
 from models.users import UserORM
 from shared.db.async_session import get_async_db
+from schemas.token_schema import TokenPayload
+
 
 
 
 password_hash = PasswordHash.recommended()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/users/token")
 settings = get_settings()
 
 
@@ -47,8 +51,58 @@ def create_access_token(username: str, email: str, expires_delta: timedelta | No
     else:
         expire = datetime.now(timezone.utc) + timedelta(minutes=15)
 
-    to_encode.update({"exp": expire.isoformat()})
+    # converting date -> timestamp -> int
+    to_encode.update(dict(exp=int(expire.timestamp())))
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
 
+
+async def get_current_user(
+        token: Annotated[str, Depends(oauth2_scheme)],
+        db: Annotated[AsyncSession, Depends(get_async_db)],
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        username = payload.get("username")
+        email = payload.get("email")
+
+        if username is None or email is None:
+            raise credentials_exception
+
+        token_data = TokenPayload(
+            username=username,
+            email=email,
+        )
+
+    except ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token has expired",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    except DecodeError as e:
+        # Specific handling for the exp integer issue
+        print(f"Decode error: {e}")  # For debugging
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token has invalid format",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    except InvalidTokenError:
+        raise credentials_exception
+
+    stmt = await db.execute(
+        select(UserORM).filter(UserORM.username == token_data.username)
+    )
+    user = stmt.scalars().first()
+    if user is None:
+        raise credentials_exception
+
+    return user
 
